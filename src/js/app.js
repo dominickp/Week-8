0b12fc68f8498b1880b0279bbec9811cce617bbb
Dominick Peluso <peluso.dominick@gmail.com>
2015-10-21 19:57:00 -0400

    Basic model in form

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 var app = angular.module('logintrest', []);
 
-app.controller('FormController', ['$scope', function($scope){
-  $scope.submitForm = function(){
-    console.log($scope.loginForm);
-    
-  };
-}]);
+
