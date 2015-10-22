916e65a2bc6e044ce0fc2a8e0fdd25a09582f6e3
Dominick Peluso <peluso.dominick@gmail.com>
2015-10-21 20:21:29 -0400

    Setup login action

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,23 @@
-var app = angular.module('logintrest', []);
+angular.module('logintrest', [])
+    .controller('FormController', function(){
+
+      var vm = this;
+
+      //vm.username = "John Harvard";
+
+      vm.logins = [];
+
+      vm.login = function(){
+        console.log("loggin in");
+
+        // Push into username array
+        vm.logins.push({username: vm.username, password: vm.password, time: new Date()});
+        console.log(vm.logins);
+
+        // Empty values
+        vm.username = "";
+        vm.password = "";
+      };
+    });
 
 
